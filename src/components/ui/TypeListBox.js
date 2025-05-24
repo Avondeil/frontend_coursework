1c737648ea5d865433e5b3488d8b9ce19e072c4b
Avondeil <kirill.malugin24@gmail.com>
2025-05-24 12:34:14 +0500

    Исправил проблему с изображениями на главной странице

--- src/components/ui/TypeListBox.js
+++ src/components/ui/TypeListBox.js
@@ -19,25 +19,22 @@ const TypeListBox = () => {
 
             <div className="catalog-types">
                 <div className="catalog-item" onClick={() => handleCategoryClick("autoboxes")}>
-                    <img src="https://eurodetal.pro/upload/resize_cache/iblock/f92/500_500_140cd750bba9870f18aada2478b24840a/i2qoirk01b9zlq5shd7fsk1euuysi967.jpg" alt="Автобокс"/>
+                    <img src="https://www.rackworld.ru/upload/iblock/fd4/i2ima2ntz1xcttg20sz5wpnbnxz7vq04/avtomobilnyy-boks-taurus-altro-460-cb-chernyy-karbon-.png" alt="Автобокс"/>
                     <h4>Автобоксы ЕКБДеталь</h4>
-                    <p>10 товаров</p>
                     <p className="catalog-description">Автобоксы от ЕКБДеталь</p>
                 </div>
                 <div className="catalog-item" onClick={() => handleCategoryClick("roof_racks")}>
                     <img
-                        src="https://eurodetal.pro/upload/resize_cache/iblock/ea5/500_500_140cd750bba9870f18aada2478b24840a/43ucy21lqz01bsage6x0m80igegg5fww.JPEG"
+                        src="https://le96.ru/upload/iblock/4f8/2flyib0ot809qf12zx44nlywcxaboamz.png"
                         alt="Багажники"/>
                     <h4>Багажники ЕКБДеталь</h4>
-                    <p>62 товара</p>
                     <p className="catalog-description">Багажники от ЕКБДеталь</p>
                 </div>
                 <div className="catalog-item" onClick={() => handleCategoryClick("parts_accessories")}>
                     <img
-                        src="https://eurodetal.pro/upload/resize_cache/iblock/c18/500_500_140cd750bba9870f18aada2478b24840a/el6or4gatw0vp01avo9f9q7k82sf0c3y.JPEG"
+                        src="https://app.miles-auto.com/uploads/categories/9_67dd6e08c1879.png"
                         alt="Запчасти"/>
                     <h4>Запчасти и аксессуары ЕКБДеталь</h4>
-                    <p>49 товаров</p>
                     <p className="catalog-description">Запчасти и аксессуары от ЕКБДеталь</p>
                 </div>
             </div>
